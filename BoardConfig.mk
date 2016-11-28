1c308eca2c045003327acd203c5d9369f2cb7a10
c457 <android.c357@gmail.com>
2016-11-28 16:14:52 -0600

    bowser: Update wifi configuration
    * Intel cherrytrail

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,15 +82,11 @@ BOARD_SEPOLICY_UNION += \
 
 # Wifi
 BOARD_WLAN_DEVICE                := rtl
-BOARD_HAVE_REALTEK_WIFI          := true
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
-WPA_SUPPLICANT_VERSION           := VER_0_8_X
+WPA_SUPPLICANT_VERSION           := VER_2_1_DEVEL
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_rtl
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_rtl
-WIFI_DRIVER_FW_PATH_PARAM        := ""
-WIFI_DRIVER_FW_PATH_AP           := ""
-WIFI_DRIVER_FW_PATH_STA          := ""
 
 TW_THEME := landscape_hdpi
 BOARD_HAS_NO_REAL_SDCARD := true
