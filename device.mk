dcd562d7a0b5147c1e53ea73af275b11aceee27a
Yifan Hong <elsk@google.com>
2017-03-22 10:56:07 -0700

    bowser: Enable graphics allocator binderized service.
    Test: camera taking picture with different modes / video works
    Test: Youtube video plays
    Test: maps work
    Test: lshal doesn't show extra pids for passthrough graphics allocator.
    Bug: 36462585
    Change-Id: I531cef3660a0fbb3c7eaf55b8f2b1ae2458d2878

--- device.mk
+++ device.mk
@@ -87,7 +87,10 @@ PRODUCT_PACKAGES += \
 
 # Graphics
 PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
     gralloc.tegra \
     hwcomposer.tegra
 
