239e91941a3a23d46fd700f774049f0c4810ead4
Mikhail Naganov <mnaganov@google.com>
2017-06-30 14:03:44 -0700

    bowser: Remove Broadcast Radio HAL impl library
    There is no support for FM radio on this device.
    
    Bug: 33050893
    Change-Id: I09667460f258750e0e7b5502fc98e045db906f2e
    Test: audio works, no impl lib in /vendor/lib[64]/hw

--- device.mk
+++ device.mk
@@ -52,7 +52,6 @@ $(call inherit-product, frameworks/native/build/tablet-10in-xhdpi-2048-dalvik-he
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
