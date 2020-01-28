39e653525299cb1680c751b5a2adfa667a4037a8
C457 <android.c357@gmail.com>
2020-01-28 17:30:37 -0600

    bowser: vendor/cm --> vendor/lineage

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/hp/bowser/full_bowser.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="hp/bowser/200a:4.4.2/4.4.2-17r25.1-11-11/17r25.1-11-11:user/release-keys" \
