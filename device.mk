32974c7d84d3a81a78477239d412b6e5578a4dff
C457 <android.c357@gmail.com>
2020-02-01 00:58:33 -0600

    bowser: Switch to passthrough for graphics allocator

--- device.mk
+++ device.mk
@@ -88,7 +88,6 @@ PRODUCT_PACKAGES += \
 # Graphics
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     gralloc.tegra \
